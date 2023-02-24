e6acfd0bdb775203f90b9fe345a1e705f9b6d2f3
mrcasareno <93808345+mrcasareno@users.noreply.github.com>
2023-02-23 18:30:17 -0800

    sTUFF
    Added gradient to header + text

---
+++ BetterEats/components/ProductButton.js
@@ -0,0 +1,14 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+
+function ProductButton(){
+    function pressHandler(){
+        console.log('Pressed!')
+    }
+    
+}
+
+export default ProductButton;
+
+const styles = StyleSheet.create({
+
+});
--- BetterEats/screens/UserPreferences.js
+++ BetterEats/screens/UserPreferences.js
@@ -1,10 +1,22 @@
-import {Text, Pressable, StyleSheet, View} from "react-native";
+import { Text, Pressable, StyleSheet, View } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
 
-function UserPreferences(){
+function UserPreferences() {
     return (
-        <View style={styles.header}>
-            <Text style={styles.headerText} >bettereats</Text>
+        <View>
+            <LinearGradient colors={['#54a86b','#dff7e5']}>
+                <View style={styles.header}>
+                    <Text style={styles.headerText} >bettereats</Text>
+                </View>
+            </LinearGradient>
+            <View style={styles.textContainer}>
+                <Text style={styles.categoryText}>Preferred Products:</Text>
+            </View>
+
+            <View style={styles.productsContainer}></View>
+
         </View>
+
     );
 }
 
@@ -12,26 +24,34 @@ export default UserPreferences;
 
 const styles = StyleSheet.create({
     header: {
-        backgroundColor: '#cccccc',
-        fontSize: 50,
         padding: 16,
-        
-        
     },
     headerText: {
         color: 'black',
         fontSize: 50,
         textAlign: "center",
-        marginTop: 30
+        marginTop: 30,
+        fontWeight: '500'
+    },
+    categoryText: {
+        color: '#black',
+        fontSize: 20,
+        textAlign: 'center',
+        fontWeight: '500'
+
+    },
+    textContainer: {
+        marginTop: 25,
+        marginHorizontal: 50,
+        alignItems: 'center'
     },
     productsContainer: {
         padding: 16,
-        marginTop: 100,
-        marginHorizontal: 24,
-        backgroundColor: 'white',
+        marginTop: 30,
+        backgroundColor: '#eeeeee',
         borderRadius: 8,
     },
-    buttonsContainer:{
+    buttonsContainer: {
         flexDirection: 'row'
     },
     buttonContainer: {
