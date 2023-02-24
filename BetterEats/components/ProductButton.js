394179a2e11998670772895aa2a7af1096334191
mrcasareno <93808345+mrcasareno@users.noreply.github.com>
2023-02-23 19:25:26 -0800

    Product Button
    Added ProductButton component and used for UserPreferences screen

--- BetterEats/components/ProductButton.js
+++ BetterEats/components/ProductButton.js
@@ -1,14 +1,49 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
+import {useState} from 'react';
 
-function ProductButton(){
-    function pressHandler(){
+function ProductButton(props) {
+    function pressHandler() {
         console.log('Pressed!')
     }
-    
+    return (
+        <View style={styles.buttonOuterContainer}>
+            <Pressable
+                onPress={pressHandler}
+                style={( {pressed}) => pressed
+                    ? [styles.buttonInnerContainer, styles.pressed]
+                    : styles.buttonInnerContainer}
+            >
+                <Text style={styles.buttonText}>{props.children}</Text>
+            </Pressable>
+        </View>
+
+    );
 }
 
 export default ProductButton;
 
 const styles = StyleSheet.create({
-
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: 'hidden'
+    },
+    buttonInnerContainer: {
+        backgroundColor: '#cccccc',
+        paddingVertical: 8,
+        paddingHorizontal: 8,
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+    },
+    buttonText: {
+        color: 'black',
+        textAlign: 'center',
+        fontSize: '15'
+    },
+    pressed: {
+        opacity: 0.75,
+        backgroundColor: '#403f3f'
+    }
 });
--- BetterEats/screens/UserPreferences.js
+++ BetterEats/screens/UserPreferences.js
@@ -1,7 +1,9 @@
 import { Text, Pressable, StyleSheet, View } from "react-native";
 import { LinearGradient } from 'expo-linear-gradient';
+import ProductButton from "../components/ProductButton";
 
 function UserPreferences() {
+
     return (
         <View>
             <LinearGradient colors={['#54a86b','#dff7e5']}>
@@ -13,8 +15,54 @@ function UserPreferences() {
                 <Text style={styles.categoryText}>Preferred Products:</Text>
             </View>
 
-            <View style={styles.productsContainer}></View>
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <ProductButton>Fruits</ProductButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <ProductButton>Vegetables</ProductButton>
+                </View>
+                
+            </View>
 
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <ProductButton>Pet Food</ProductButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <ProductButton>Baked Goods</ProductButton>
+                </View>
+                
+            </View>
+
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <ProductButton>Body Care</ProductButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <ProductButton>Juices</ProductButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <ProductButton>Bread</ProductButton>
+                </View>
+
+            </View>
+
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <ProductButton>Crafts</ProductButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <ProductButton>Flowers</ProductButton>
+                </View>
+                
+            </View>
+
+            <View style={styles.textContainer}>
+                <Text style={styles.categoryText}>Preferred Payment:</Text>
+            </View>
+
+            
         </View>
 
     );
@@ -41,9 +89,10 @@ const styles = StyleSheet.create({
 
     },
     textContainer: {
-        marginTop: 25,
+        marginTop: 20,
         marginHorizontal: 50,
-        alignItems: 'center'
+        alignItems: 'center',
+        marginBottom: 10
     },
     productsContainer: {
         padding: 16,
